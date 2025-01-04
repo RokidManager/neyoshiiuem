0754cb23b069b409db3ef0d5791f638aa68255a9
RokidManager <kyoukostudio@gmail.com>
2025-01-04 20:24:16 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg install python-pip && pkg install python tsu libexpat openssl -y && pip install requests Flask colorama aiohttp psutil crypto pycryptodome prettytable loguru rich pytz websocket-client
+termux-setup-storage
+pkg update -y
+pkg install -y python python-pip tsu libexpat openssl
+
+pip install requests Flask colorama aiohttp psutil crypto pycryptodome prettytable loguru rich pytz websocket-client
+
 curl -Ls "https://raw.githubusercontent.com/RokidManager/neyoshiiuem/refs/heads/main/Rokid-Setup" -o /sdcard/Download/Rokid-UGPhone.py
+
+echo "Rokid Setup completed successfully!"
