c576d34d61e9cbc380315a4dc4b1e8976dcf9d5d
RokidManager <kyoukostudio@gmail.com>
2025-01-05 04:17:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ pkg install -y python python-pip tsu libexpat openssl
 pkg update -y
 pkg install -y python python-pip
 
-pip install requests Flask colorama aiohttp psutil crypto pycryptodome prettytable loguru rich pytz websocket-client
+pip install requests Flask colorama aiohttp psutil crypto pycryptodome prettytable loguru rich pytz websocket-client tqdm
 
 curl -Ls "https://raw.githubusercontent.com/RokidManager/neyoshiiuem/refs/heads/main/Rokid-UGPhone" -o /sdcard/Download/Rokid-UGPhone.py
 
