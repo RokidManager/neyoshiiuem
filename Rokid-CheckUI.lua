c20e39cf4d0bbd99806e819ae2c525e4315e98d8
RokidManager <kotenacc7@gmail.com>
2025-05-30 08:34:19 +0700

    Update Rokid-CheckUI.lua

--- Rokid-CheckUI.lua
+++ Rokid-CheckUI.lua
@@ -1,8 +1,1 @@
-task.spawn(function()
-    while true do
-        pcall(function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/RokidManager/neyoshiiuem/refs/heads/main/Check%20Executor"))()
-        end)
-        task.wait(1)
-    end
-end)
+print("11")
